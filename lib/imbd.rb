3adf380689e910110df391cb4e0978ede23cf3ee
ianaberbach <naberbach@gmail.com>
2015-01-17 17:55:08 -0500

    adding stuff 2

--- lib/imbd.rb
+++ lib/imbd.rb
@@ -1,10 +1,15 @@
-require 'nokogiri'
-require 'open-uri'
+  require 'nokogiri'
+  require 'open-uri'
+  require 'pry'
 
-imbd_html = open("http://www.imdb.com/genre/comedy/?ref_=gnr_mn_co_mp")
+  html = open("http://www.imdb.com/genre/comedy/?ref_=gnr_mn_co_mp")
 
-comedy = Nokogiri::HTML(imbd_html)
+  comedy = Nokogiri::HTML(html)
 
-comedy.css(".article")
+ comedy.css(".title").each do |movie_title|
+   movie_title.children.children.first.text
+ end
 
-
+ comedy.css(".image").each do |movie|
+   movie.comedy.css(".image").children[1].attributes["href"].value
+ end
