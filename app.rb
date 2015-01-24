e54b38075b7f17dfb02eae8216e42a1462289313
github username <github email address>
2015-01-24 15:26:35 -0500

    Setting up app.rb

--- app.rb
+++ app.rb
@@ -1,3 +1,16 @@
-def watermelon
-    puts "Watermelon is fantastic"
+require 'bundler'
+Bundler.require
+require_relative "./lib/imbd.rb"
+
+class MyApp < Sinatra::Base
+
+  get '/' do
+  	@scraper = Scraper.new
+    erb :index
+  end
+
+  get '/comedy' do
+  	@titles = @scraper.comedy_titles
+  end
+
 end
