defc6d1f448fedb654503a69b1f6de472c81758a
github username <github email address>
2015-01-23 21:10:05 -0500

    here is more stuff

--- lib/imbd.rb
+++ lib/imbd.rb
@@ -1,10 +1,20 @@
 require 'nokogiri'
 require 'open-uri'
 
-imbd_html = open("http://www.imdb.com/genre/comedy/?ref_=gnr_mn_co_mp")
+imdb_html = open("http://www.imdb.com/genre/comedy/?ref_=gnr_mn_co_mp")
 
 comedy = Nokogiri::HTML(imbd_html)
 
 comedy.css(".article")
 
+imdb_action = open("http://www.imdb.com/genre/action")
 
+action = Nokogiri::HTML(imdb_action)
+
+action.css(".article")
+
+imdb_horror = open("http://www.imdb.com/genre/horror")
+
+horror = Nokogiri::HTML(imdb_horror)
+
+horror.css(".article")
