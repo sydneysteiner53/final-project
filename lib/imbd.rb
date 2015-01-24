8891b76198bfee22ead65143027e97abd573be9c
bryanpalmer973 <bryanpalmer973@gmail.com>
2015-01-24 14:48:25 -0500

    something

--- lib/imbd.rb
+++ lib/imbd.rb
@@ -5,14 +5,14 @@
 class Scraper
 
     def initialize
-    comedy_html = open("http://www.imdb.com/genre/comedy/?ref_=gnr_mn_co_mp")
-    @comedy = Nokogiri::HTML(comedy_html)
-   
-    imdb_horror = open("http://www.imdb.com/genre/horror")
-    @horror = Nokogiri::HTML(imdb_horror)
+        comedy_html = open("http://www.imdb.com/genre/comedy/?ref_=gnr_mn_co_mp")
+        @comedy = Nokogiri::HTML(comedy_html)
+       
+        imdb_horror = open("http://www.imdb.com/genre/horror")
+        @horror = Nokogiri::HTML(imdb_horror)
 
-    imdb_action = open("http://www.imdb.com/genre/action")
-    @action = Nokogiri::HTML(imdb_action)
+        imdb_action = open("http://www.imdb.com/genre/action")
+        @action = Nokogiri::HTML(imdb_action)
     end
 
     def comedy_titles
@@ -23,19 +23,15 @@ class Scraper
         title
     end
 
-    def horror_picures
-        picture = []
-    @horror.css(".image").each do |movie_picture|
-        picture << movie_picture.css("a img")
+    def horror_pictures
+         picture = []
+        @horror.css(".image").each do |movie_picture|
+            picture << movie_picture.css("a img")
+        end
+        picture
     end
-     picture
-    end
 
-puts title
-
-# action.css(".article")
-# horror.css(".article")
-
+end
 
 
 
